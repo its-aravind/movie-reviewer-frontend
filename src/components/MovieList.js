242bb6b1837ae7037c6f3f09a38c3b83136e7c58
Aravind Kumar <131791315+its-aravind@users.noreply.github.com>
2024-07-13 02:49:02 +0530

    Update MovieList.js

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -11,7 +11,7 @@ const MovieList = () => {
   useEffect(() => {
     const fetchMovies = async () => {
       try {
-        const response = await axios.get('http://localhost:9000/movies/');
+        const response = await axios.get(https://movie-reviewer-backend.onrender.com/movies/');
         setMovies(response.data);
       } catch (error) {
         console.error('Error fetching movies:', error);
